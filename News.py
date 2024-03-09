c1b1aef07cc46c732e201ad8c9e38b8d36fd7708
Rajat Malviya <143414044+rajat-malvi@users.noreply.github.com>
2024-03-09 21:55:49 +0530

    Update News.py

--- News.py
+++ News.py
@@ -1,28 +1,28 @@
-from flask import Flask, render_template, url_for, redirect, session,request
-import psycopg2 
-from nltk import *
+from flask import Flask, render_template, url_for, redirect, session,request       # It make a flask environment using some function 
+import psycopg2                                                                    # For postgres sql data base 
+from nltk import *                                                                 # Hepl to analyse the text 
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-# nltk.download('vader_lexicon')
-import json
-import requests
-# from urllib import request
-import re
-from bs4 import BeautifulSoup
-from authlib.integrations.flask_client import OAuth
+from nltk.sentiment.vader import SentimentIntensityAnalyzer                        # Sentiment analysis using vander class 
+import json                                                                        # It help to manipulate json 
+import requests              
+import re                                                                          # Regular expression operations
+from bs4 import BeautifulSoup                                                      # It screpe news from website   
+from authlib.integrations.flask_client import OAuth                                # OAuth integration for Flask
 
-app=Flask(__name__,static_folder='static')
+# Here instence made  
+app=Flask(__name__,static_folder='data')
 
-# oAuth acount
+# oAuth instance
 oauth = OAuth(app)
 
-# oautherized id
+# oautherized GitHub id
 app.config['SECRET_KEY'] = "THIS SHOULD BE SECRET"
 app.config['GITHUB_CLIENT_ID'] = "992ca9ab81fae0231b83"
 app.config['GITHUB_CLIENT_SECRET'] = "a0c7e13b2dfdb91e32c700563eed8420f6594e2b"
 github_admin_usernames = [ "atmabodha","rajat-malvi"]
 
-# registration id 
+
+#  regarding registration  
 github = oauth.register(
     name='github',
     client_id=app.config["GITHUB_CLIENT_ID"],
@@ -38,12 +38,13 @@ github = oauth.register(
 
 # backend (psycopg2)
 def connect_to_db():
+    ''' this function for data base connection'''
     conn=psycopg2.connect(
         host='dpg-cnlhmc0l6cac73ef0vmg-a',  database='dhp2024_44yk', user='dhp2024_44yk_user', password='hYblUsnTd53xOGdkVu0d70jAP5LR1SBC')
     return conn
-    
+
+# It create a table if not exist  
 def create_table():
-    # conn = psycopg2.connect(**db_params)
     conn=connect_to_db()
     cursor = conn.cursor()
     
@@ -60,41 +61,46 @@ def create_table():
             )
         """)
     conn.commit()
-# create_table()
-
-
-
+    
+    
 # ---------------------------------------------------------------------------------------------------------------------------------------
 
-# bs4 code
+
+# bs4 code 
 def getsoup(s):
+    '''it provide soup(main )'''
     URL=f'{s}'
     page=requests.get(URL)
     soup=BeautifulSoup(page.content,"html.parser")
     return soup
 
 def reax(s):
+    '''Regular expresion it clean the text in some spacific pattern'''
     raw_html=str(s)
-    cleantext=re.sub(r'<.*?>','',raw_html)
+    cleantext=re.sub(r'<.*?>','',raw_html)      # sub use to substitute the value with other
     return cleantext
 
 # www.thehindu.com
 def thd(s):
     soup=getsoup(s)
     # for purely pera
-    artical = soup.find('div', class_='articlebodycontent')
-    lst = artical.find_all('p')
+    artical = soup.find('div', class_='articlebodycontent') # hear crticle accese 
+    lst = artical.find_all('p')     # list of all peragraph 
 
     cleaned_news = []
+    # itrete all pere graph and clean all peregraph text by using some patterns 
     for tag in lst:
         if 'share' not in tag.get('class', []) and 'related-topics-list' not in tag.get('class', []) and 'comments' not in tag.get('class', []):
             cleaned_news.append(tag.get_text().strip())
     pera=''
+    # again combain all peragraph and make a article.
     for news in cleaned_news:
         pera+=" " + news
     return pera.strip()
 
+
 def thddict(l):
+    '''The Hindu dict it return some important detail that extrect from the article  '''
     soup=getsoup(l)
     string=soup.script.get_text().strip()
     dict1=json.loads(string[55:-2].replace('\n',"").replace('// when available',"").replace("'",'"'))
@@ -103,39 +109,33 @@ def thddict(l):
 
 # TOI
 def toi(s):
-    # dict return
+    # It return a json in which contain all meta data
     soup=getsoup(s)
     scriptlist=soup.find_all('script')    
     dict1=json.loads(scriptlist[-2].get_text())
     return dict1
 
-# ---------------------------------------------------------------------------------------------------------------------------------------
 
-# NLTK
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# NLTK 
 def sentence_func(s):
+    '''Using sent_tokenize function it return a list of sentence'''
     sentence= sent_tokenize(s) 
     return len(sentence)
 
 def word_func(s):
+    '''It use word_tokenize and return a list of word without using punctuation.'''
     lst=[',','.','?','!']
     word_list=word_tokenize(s)
     count=0
     for i in word_list:
         if i not in lst:
-            count+=1
+            count+=1   
     return count
 
-# def s_word(s):
-#     lst=nltk.corpus.stopwords.words('english')
-#     count=0
-#     word_list=word_tokenize(s)
-#     for i in word_list:
-#         if i in lst:
-#             count+=1
-#     return count
-
 def upos1(s):
-    # it tell about noun, verb.
+    # it tell about noun, verb,adjective etc.
+    '''this function return a dict in  '''
     words=word_tokenize(s)
     lst = nltk.pos_tag(words,tagset='universal') # in this list we have tuple that indicate word is verb, noun or somthing else
     dict1={}
@@ -244,12 +244,11 @@ def portal():
             dictmain['articleTag']=articleTag
             dictmain['authername']=authename
             # backend save
-            cur.close()
             conn.close()
    
         return render_template('News.html',dictmain=dictmain,pera=pera,name=name)
     except Exception as e:
-        return render_template('News.html',dictmain=dictmain,pera=pera,name=name)
+        return render_template('News.html',dictmain=dictmain,name=name)
 
 
 # Github login route
@@ -290,3 +289,16 @@ def github_logout():
 if __name__=='__main__':
     create_table()
     app.run(debug=True)
+
+    
+# 1. Extract the news text and clean it.
+
+# 2. Analyse the text for number of sentences, words, POS tags, and any other relevant information. Display this on the website in a nicely formatted manner after the URL is submitted.
+
+# 3. Store the URL, news text, and a summary of your analysis in a PostgreSQL table.
+
+# 4. There should be a button on the website to view history of URLs submitted and the associated analysis done. This should only be visible and accessible by the website admin.
+
+# 5. Host this website and PostgreSQL database on www.render.com [free tier of 3 months]. Do not enter your bank information anywhere.
+
+# 6. Create a detailed PDF report of the algorithm used and show output for a few test cases.
